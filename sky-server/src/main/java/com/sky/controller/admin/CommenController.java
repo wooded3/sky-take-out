f5c558e05941d73fd3f94f505f50425933bc36af
wooded <wooded@qq.com>
2025-12-27 16:02:04 +0800

    新增菜品 12/27

--- sky-server/src/main/java/com/sky/controller/admin/CommenController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommenController.java
@@ -1,4 +1,49 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 通用接口
+ */
+@Slf4j
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
 public class CommenController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+    /**
+     *文件上传
+     */
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传：{}", file);
+
+        try {
+            //原始文件名
+            String originalFilename = file.getOriginalFilename();
+            String substring = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + substring;
+
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e.getMessage());
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
 }
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,10 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+
+/**
+ * 新建菜品
+ */
+public interface DishService {
+    void save(DishDTO dishDTO);
+}
